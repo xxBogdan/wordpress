411b3bb0a8ada0815dd99798dd0e08c36c5adce2
xxBogdan <poprik.bogdan2014@yandex.ru>
2021-04-21 20:10:41 +0400

    ..

---
+++ wp-content/themes/twentynineteen/bp_build.php
@@ -0,0 +1,19 @@
+<?php /*
+* Template Name: bp_build
+* Template Post Type: post,page
+*/
+?>
+<!doctype html>
+<html>
+<head>
+<?php wp_head(); ?>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+<title>ReactJavaScript</title>
+<link href="<?php echo esc_url ( get_template_directory_uri() ); ?>/bp_build_reactjs/static/css/main.e61a2d79.css" rel="stylesheet">
+</head>
+<body>
+<div id="root"></div>
+<script type="text/javascript" src="<?php echo esc_url ( get_template_directory_uri() ); ?>/bp_build_reactjs/static/js/main.a5742c08.js"></script>
+</body>
+</html>
