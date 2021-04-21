cc2b695af1918a0b95b759827d0f231d4d03048a
xxBogdan <poprik.bogdan2014@yandex.ru>
2021-04-21 19:40:43 +0400

    Update БП_build.php

--- wp-content/themes/twentynineteen/БП_build.php
+++ wp-content/themes/twentynineteen/БП_build.php
@@ -1,4 +1,14 @@
-<!doctype html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"><title>ReactJavaScript</title><link href="/static/css/main.e61a2d79.css" rel="stylesheet"></head><body><div id="root"></div><script type="text/javascript" src="/static/js/main.a5742c08.js"></script></body></html>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+<title>ReactJavaScript</title><link href="/static/css/main.e61a2d79.css" rel="stylesheet">
+</head><body><div id="root">
+</div>
+<script type="text/javascript" src="/static/js/main.a5742c08.js"></script>
+</body>
+</html>
 <?php 
  * Template Name: БП_build
  * Template Post Type: post
