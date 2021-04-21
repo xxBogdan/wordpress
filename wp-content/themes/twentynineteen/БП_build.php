29c8c1ea58eb7612a61668865c07dd8ecd781183
xxBogdan <poprik.bogdan2014@yandex.ru>
2021-04-21 19:50:17 +0400

    Update БП_build.php

--- wp-content/themes/twentynineteen/БП_build.php
+++ wp-content/themes/twentynineteen/БП_build.php
@@ -8,7 +8,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-<title>ReactJavaScript</title><link href="/static/css/main.e61a2d79.css" rel="stylesheet">
+<title>ReactJavaScript</title><link href="БП_build_reactjs/static/css/main.e61a2d79.css" rel="stylesheet">
 </head><body><div id="root">
 </div>
 <script type="text/javascript" src="<?php echo esc_url ( get_template_directory_url() ); ?>/БП_build_reactjs/static/js/main.a5742c08.js"></script>
