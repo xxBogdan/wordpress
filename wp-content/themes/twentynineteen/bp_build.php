ba199c47e593bd8c7703b35999ca6f648efd100c
xxBogdan <poprik.bogdan2014@yandex.ru>
2021-04-28 19:37:07 +0400

    ...

--- wp-content/themes/twentynineteen/bp_build.php
+++ wp-content/themes/twentynineteen/bp_build.php
@@ -14,6 +14,6 @@
 </head>
 <body>
 <div id="root"></div>
-<script type="text/javascript" src="<?php echo esc_url ( get_template_directory_uri() ); ?>/bp_build_reactjs/static/js/main.a5742c08.js"></script>
+<script type="text/javascript" src="<?php echo esc_url ( get_template_directory_uri() ); ?>/bp_build_reactjs/static/js/main.cf838536.js"></script>
 </body>
 </html>
